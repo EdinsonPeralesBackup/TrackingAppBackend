7112d22578ee207676fb4de21a87ddbfcdb6f844
Ghiro Fernando Luck Herrera <ghiroluck@hotmail.com>
2026-01-14 20:14:57 -0500

    Correcion de formato geografico

--- Tracking.Infrastructure/Services/MapsServices.cs
+++ Tracking.Infrastructure/Services/MapsServices.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,7 +23,7 @@ namespace Tracking.Infrastructure.Services
         public async Task<string> ObtenerRuta(RegisterRouteCommand command)
         {
             using var client = new HttpClient();
-            string url = $"https://maps.googleapis.com/maps/api/directions/json?origin={command.Origin.Lat},{command.Origin.Lng}&destination={command.Destination.Lat},{command.Destination.Lng}&key={apiKey}";
+            string url = $"https://maps.googleapis.com/maps/api/directions/json?origin={(command.Origin.Lat).ToString(CultureInfo.InvariantCulture)},{(command.Origin.Lng).ToString(CultureInfo.InvariantCulture)}&destination={(command.Destination.Lat).ToString(CultureInfo.InvariantCulture)},{(command.Destination.Lng).ToString(CultureInfo.InvariantCulture)}&key={apiKey}";
             var response = await client.GetStringAsync(url);
             return response;
         }
